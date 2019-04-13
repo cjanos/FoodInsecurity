789d18ea8d234188f5baba17347075260341cbc5
Ryan <phantasmalspectre@aol.com>
2019-04-12 20:48:01 -0400

    dsf

---
+++ Food Insecurity Game Jam/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> items = new List<Item>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
